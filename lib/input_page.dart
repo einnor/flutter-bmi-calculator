de0ad46e1e478c2d18fc62cd0fa21015c7a7416d
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-17 18:47:52 +0300

    Refactor

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF101E33);
+const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -53,7 +54,7 @@ class _InputPageState extends State<InputPage> {
             ],
           )),
           Container(
-            color: Color(0xFFEB1555),
+            color: bottomContainerColor,
             margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
             height: bottomContainerHeight,
