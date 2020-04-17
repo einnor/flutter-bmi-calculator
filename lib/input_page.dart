106505945b01e3bfe39ae15669f20dca2a667747
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-17 18:03:41 +0300

    Refactor

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const bottomContainerHeight = 80.0;
+const cardBackgroundColor = 0xFF101E33;
 
 class InputPage extends StatefulWidget {
   @override
@@ -21,19 +22,19 @@ class _InputPageState extends State<InputPage> {
             children: <Widget>[
               Expanded(
                 child: ReusableCard(
-                  color: Color(0xFF101E33),
+                  color: Color(cardBackgroundColor),
                 ),
               ),
               Expanded(
                 child: ReusableCard(
-                  color: Color(0xFF101E33),
+                  color: Color(cardBackgroundColor),
                 ),
               ),
             ],
           )),
           Expanded(
             child: ReusableCard(
-              color: Color(0xFF101E33),
+              color: Color(cardBackgroundColor),
             ),
           ),
           Expanded(
@@ -41,12 +42,12 @@ class _InputPageState extends State<InputPage> {
             children: <Widget>[
               Expanded(
                 child: ReusableCard(
-                  color: Color(0xFF101E33),
+                  color: Color(cardBackgroundColor),
                 ),
               ),
               Expanded(
                 child: ReusableCard(
-                  color: Color(0xFF101E33),
+                  color: Color(cardBackgroundColor),
                 ),
               ),
             ],
