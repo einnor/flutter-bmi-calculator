6ed2179479da19f563cb3f3ef7f34df62d867524
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-20 17:57:46 +0300

    More refactoring and renaming

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-const labelTextStyle = TextStyle(
+const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF8D8E98),
 );
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xFF101E33);
+const kInactiveCardColor = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
