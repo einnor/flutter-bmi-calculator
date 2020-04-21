fce98404ce0d80c9cd07b2cb2d947e30a696f486
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-21 17:13:44 +0300

    Specifying the width and height

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -181,6 +181,10 @@ class RoundIconButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
       shape: CircleBorder(),
       fillColor: Color(0xFF4C4F5E),
     );
