90c017e303caefa32070fe4fb5d1548dbb272ef6
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-21 19:23:57 +0300

    Method to calculate BMI

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,6 +1,13 @@
+import 'dart:math';
+
 class Calculator {
   Calculator({this.height, this.weight});
 
   final int height;
   final int weight;
+
+  String calculateBMI() {
+    double bmi = weight / pow(height / 100, 2);
+    return bmi.toStringAsFixed(1);
+  }
 }
