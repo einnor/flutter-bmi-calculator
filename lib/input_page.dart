07e985490c183dc81df036f98d9595aa44bc0b24
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-21 18:25:16 +0300

    Style the calculate button

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -215,6 +215,7 @@ class _InputPageState extends State<InputPage> {
             child: Container(
               color: kBottomContainerColor,
               margin: EdgeInsets.only(top: 10.0),
+              padding: EdgeInsets.only(bottom: 20.0),
               width: double.infinity,
               height: kBottomContainerHeight,
               child: Center(
